84f1d03f4631f287f17f6e5d0d1fca8d0ecf8dad
JEONGYOUNGGYU <zkdlf90@gmail.com>
2024-01-23 11:10:44 +0900

    jpql 사용

--- src/main/java/com/example/crudstudy/board/CommentRepository.java
+++ src/main/java/com/example/crudstudy/board/CommentRepository.java
@@ -3,13 +3,14 @@ package com.example.crudstudy.board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
   @Modifying
   @Query("delete from Comment c where c.post.id = :postId")
   void deleteAllPostId(Long postId);
-
   // jpa -> jpql -> sql -> db
   // table 기준 jpql 엔티티 기준
   // querydsl -> jpql -> sql ->
